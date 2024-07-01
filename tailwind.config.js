8fe294b7d74361341443c959913083c9977143fa
Preetam Jain <ppj36825@uga.edu>
2024-07-01 19:15:02 -0400

    First Deployment

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/templates/index.html', './src/js/app.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
